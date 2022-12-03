e3becfac943e809a494603cfdee6462c777a726b
Lemashon <lemakipeno@gmail.com>
2022-12-04 00:31:18 +0300

    second commit

---
+++ Data Structures/merge_sort.py
@@ -0,0 +1,11 @@
+def merge_sort(list):
+    """
+    Sorts a list in ascending order
+    Returns a new sorted list
+    
+    Divide: find the midpoint of the list and divide into sublists
+    conquer: Recursively sort the sublists created in previous step
+    Combine: Merge the sorted sublists created in previous step
+    
+    """
+    
